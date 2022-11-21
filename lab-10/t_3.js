169218b0968fe2432ba6cf687e3b4313c1acd79a
Adil <adilzhapar2002@gmail.com>
2022-11-21 15:08:32 +0600

    upd

---
+++ lab-10/t_3.js
@@ -0,0 +1,35 @@
+let register = document.querySelector('.btn-register');
+let login = document.querySelector('.btn-login');
+
+function CheckPassword(inputtxt) {
+    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if (inputtxt.match(passw)) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+register.addEventListener('click', () => {
+    let username = document.querySelector('.login').value;
+    let password = document.querySelector('.password').value;
+    if(CheckPassword(password)){
+        window.localStorage.setItem(username, password);
+        location.replace("./logged.html");
+    }else{
+        alert("Your password must include at least one Uppercase, lowercase and digit");
+
+    }
+
+})
+
+login.addEventListener('click', () => {
+    let username = document.querySelector('.login').value;
+    let password = document.querySelector('.password').value;
+    let logged = window.localStorage.getItem(username);
+    if (logged === password) {
+        location.replace("./logged.html")
+    } else {
+        alert("wrong credentials");
+    }
+});
